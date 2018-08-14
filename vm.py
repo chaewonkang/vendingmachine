13114f8c253a56fe93a6d5d8b76729d7db5e200b
Chaewon Kang <saturday.the17th@gmail.com>
2018-08-14 15:40:01 +0900

    나의도움이다

--- vm.py
+++ vm.py
@@ -1,9 +1,22 @@
+change = 0
+
+
+def init():
+    global change
+    change = 0
+
+
 def run(raw):
+    global change
+
     tokens = raw.split(" ")
-    cmd = toens[0]
+    cmd, params = tokens[0], tokens[1:]
 
     if cmd == "잔액":
-        return "잔액은 100원입니다"
+        return "잔액은 " + str(change) + "원입니다"
     else:
-        coin = tokens[1]
+        coin = params[0]
+        change += int(coin)
         return coin + "원을 넣었습니다"
+    else:
+        return "알 수 없는 명령입니다"
